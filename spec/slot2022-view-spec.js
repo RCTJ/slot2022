5595e41d82a924b4e8b79d1028e03656850afd34
SBOAAAA <fun_pu@hotmail.com>
2022-11-16 16:24:14 +0700

    edit

---
+++ spec/slot2022-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Slot2022View from '../lib/slot2022-view';
+
+describe('Slot2022View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
